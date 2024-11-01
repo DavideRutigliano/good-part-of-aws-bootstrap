a5c2b866e557c9cccc2a6da034cfdb38b2d191c5
Davide Rutigliano <davide.rutigliano@ericsson.com>
2024-11-01 13:28:59 +0100

    Add launch template and multiple ec2 instances

--- scripts/delete-infra.sh
+++ scripts/delete-infra.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-REGION=us-east-1
+REGION=eu-central-1
 CLI_PROFILE=awsbootstrap
 STACK_NAME=good-part-of-aws-bootstrap
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text`
+CODEPIPELINE_BUCKET="codepipeline-$REGION-$AWS_ACCOUNT_ID"
+
+# Empty s3 bucket
+echo -e "\n\n=========== Emptying s3 bucket ==========="
+aws s3 rm s3://$CODEPIPELINE_BUCKET/* --recursive --profile $CLI_PROFILE
+
+# Delete the CloudFormation setup stack
+echo -e "\n\n=========== Deleting setup stack ==========="
+aws cloudformation delete-stack \
+    --profile $CLI_PROFILE \
+    --region $REGION \
+    --stack-name $STACK_NAME-setup
 
 # Delete the CloudFormation stack
 echo -e "\n\n=========== Deleting stack ==========="
 aws cloudformation delete-stack \
     --profile $CLI_PROFILE \
+    --region $REGION \
     --stack-name $STACK_NAME
