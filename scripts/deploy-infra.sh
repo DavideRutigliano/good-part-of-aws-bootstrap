700ffb03c1799dfa2ad04df040c1dd91cf9bf1dc
Davide Rutigliano <davide.rutigliano@ericsson.com>
2024-11-01 12:20:47 +0100

    Add CodeDeploy/CodeBuild/CodePipeline

--- scripts/deploy-infra.sh
+++ scripts/deploy-infra.sh
@@ -10,27 +10,27 @@ GH_BRANCH=main
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-demo-token)
 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text`
-CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+CODEPIPELINE_BUCKET="codepipeline-$REGION-$AWS_ACCOUNT_ID"
 
 # Deploy the CloudFormation template for CodePipeline S3 bucket
-echo -e "\n\n=========== Deploying setup.yml ==========="
+echo -e "\n\n=========== Deploying setup.yaml ==========="
 aws cloudformation deploy \
     --profile $CLI_PROFILE \
     --stack-name $STACK_NAME-setup \
     --region $REGION \
-    --template-file ctf/setup.yaml \
+    --template-file cft/setup.yaml \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template for the app infra
-echo -e "\n\n=========== Deploying main.yml ==========="
+echo -e "\n\n=========== Deploying main.yaml ==========="
 aws cloudformation deploy \
     --profile $CLI_PROFILE \
     --stack-name $STACK_NAME \
     --region $REGION \
-    --template-file ctf/main.yaml \
+    --template-file cft/main.yaml \
     --no-fail-on-empty-changeset \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
